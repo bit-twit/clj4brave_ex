624c8cc92e37b15a7bfc35bcef8024a0fd204e9f
iulian.plesoianu <bit_twit@yahoo.com>
2015-08-14 14:30:16 +0300

    Added exercises.

---
+++ src/clj4brave_ex/clj4thebrave_fct_prog.clj
@@ -0,0 +1,6 @@
+(ns clj4brave-ex.clj4thebrave-fct-prog)
+
+(defn wisdom
+  [words-of-wisdom]
+  (str words-of-wisdom ", with care! For The Queen!!!"))
+
--- src/clj4brave_ex/core.clj
+++ src/clj4brave_ex/core.clj
@@ -5,3 +5,8 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn is-palindrome
+  [string]
+  (every? #(= true %1)
+          (map #(= %1 %2) string (reverse string))))
